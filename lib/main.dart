ef91dca8c3ef132007f5b17a4548d62579970296
Satyam Srivastav <satyam.srivastav363@gmail.com>
2024-09-14 00:48:58 +0530

    SatyaDev

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/firebase_options.dart';
-import 'package:flutter_application_1/pages/displayFarmerlist.dart';
 import 'package:flutter_application_1/pages/homepage.dart';
-import 'package:flutter_application_1/pages/signup_as_merchant.dart';
 import 'package:flutter_application_1/pages/welcome.dart';
-import 'package:flutter_application_1/userwidgets/framerprofile.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.android,
+  );
   runApp(const MyApp());
 }
 
@@ -28,7 +29,36 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: FarmerList(cat: "Fruit", crop: "Orange"),
+      home:  StreamBuilder<User?>(
+                stream: FirebaseAuth.instance.authStateChanges(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return const Banner(
+                        message: '@the_extremity',
+                        location: BannerLocation.topEnd,
+                        child: HomeScreen());
+                  }
+                  if (snapshot.hasError) {
+                    return const Welcome();
+                  }
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Scaffold(
+                      body: Center(
+                        child: Column(
+                          children: [
+                            CircularProgressIndicator(),
+                            Text("Internet Seems to be slow")
+                          ],
+                        ),
+                      ),
+                    );
+                  }
+                  return const Banner(
+                      message: '@the_extremity',
+                      location: BannerLocation.topEnd,
+                      child: Welcome());
+                }),
+      
     );
   }
 }
